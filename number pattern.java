561b502b14338bdaeababdb63a12794c4f08ac74
Avani Jain <52358306+avani5jain@users.noreply.github.com>
2019-08-11 15:14:24 +0530

    Create number pattern.java

---
+++ number pattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i,j;
+		for(i=1;i<=n;i++)
+		{ for(j=1;j<=i;j++)
+		{ System.out.print(j+" ");}
+		System.out.println();
+		}
+	}
+}
